75011035d15f20cbe65c005484d9f11ac27f0804
Roen Ylagan <roenylagan@live.com>
2018-05-17 21:55:11 +0800

    at last, I got the UI fucking working

--- src/main/java/com/sgp/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/sgp/employeeservice/controller/EmployeeController.java
@@ -9,6 +9,8 @@ import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -35,6 +37,8 @@ import com.sgp.employeeservice.service.EmployeeService;
 @RequestMapping(path = "v1", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class EmployeeController {
 
+	private static Logger logger = LoggerFactory.getLogger(EmployeeController.class);
+	
 	@Autowired
 	private EmployeeService employeeService;
 
@@ -44,6 +48,7 @@ public class EmployeeController {
 	@PostMapping(path = "/employees/bulkupload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public ResponseEntity<? extends WebServiceResponse> saveEmployeesByBulk(@RequestParam("file") MultipartFile file)
 			throws IOException, EmployeeValidationException {
+		logger.info("saving data");
 		InputStream inputStream = file.getInputStream();
 		if (!file.isEmpty()) {
 			ObjectMapper mapper = new ObjectMapper();
@@ -56,6 +61,7 @@ public class EmployeeController {
 				throw new EmployeeValidationException("Errors in validating inputs", constraintValidationResults);
 			}
 			List<EmployeeResponse> responses = employeeService.persistEmployeeRequestEntries(validList);
+			logger.info("saving data completed");
 			return ResponseEntity.ok(new EmployeeListResponse(responses));
 		} else {
 			throw new IOException("the file to be processed is empty");
@@ -65,14 +71,18 @@ public class EmployeeController {
 	@GetMapping(path = "/employees/younger/than/{age}")
 	public ResponseEntity<? extends WebServiceResponse> getEmployeesYoungerThanAge(@PathVariable Integer age)
 			throws EmployeeQueryException {
+		logger.info("younger than");
 		List<EmployeeResponse> responses = employeeService.getAllEmployeesYoungerThanAge(age);
+		logger.info("younger than completed");
 		return ResponseEntity.ok(new EmployeeListResponse(responses));
 	}
 
 	@GetMapping(path = "/employees/sorted/{attribute}")
 	public ResponseEntity<? extends WebServiceResponse> sortEmployeesByAttribute(@PathVariable String attribute)
 			throws EmployeeQueryException {
+		logger.info("sorted data");
 		List<EmployeeResponse> responses = employeeService.getAllEmployeesSortedByAttribute(attribute);
+		logger.info("sorted data completed");
 		return ResponseEntity.ok(new EmployeeListResponse(responses));
 	}
 
